a50b96781e85e3c7495a50f2bc3fae1ac2451323
tejasag <tejasagarwalbly@gmail.com>
2021-03-26 15:21:28 +0530

    shiftng

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctx) {
     return MaterialApp(
-      title: "Hello World",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Hello World"),
-          backgroundColor: Color.fromRGBO(255, 100, 10, 10),
+          title: Text("Hello World App"),
+          centerTitle: true,
+          backgroundColor: Colors.pink[400],
         ),
         body: Center(
-          child: Text("Flutter is too good yee"),
+          child: Image(
+            image: NetworkImage('https://picsum.photos/500/950'),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("click"),
+          onPressed: () {},
+          backgroundColor: Colors.lightGreen,
         ),
       ),
     );
